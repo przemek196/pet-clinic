bdf2d0d92902da415d35319af3d23bc61a83166c
przemek196 <przemekbilas@gmail.com>
2021-09-14 17:04:49 +0200

    Initial Project Add

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sfg-pet-clinic
+
 Learn Spring Framework
---
+++ src/test/java/przemek/springlearn/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package przemek.springlearn.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
