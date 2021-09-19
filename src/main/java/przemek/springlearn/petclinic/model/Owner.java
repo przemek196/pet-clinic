35d231ad0e27e8fa0b0fe2de67a1eecb97ecb10f
przemek196 <przemekbilas@gmail.com>
2021-09-19 22:02:19 +0200

    create POJO model. Closes #2

---
+++ src/main/java/przemek/springlearn/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package przemek.springlearn.petclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/przemek/springlearn/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package przemek.springlearn.petclinic.model;
+
+public class Vet extends Person{
+}
