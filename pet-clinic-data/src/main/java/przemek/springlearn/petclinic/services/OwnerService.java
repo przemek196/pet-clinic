536456bdeff34d219fe8c993e5aedb638399f217
przemek196 <przemekbilas@gmail.com>
2021-10-04 20:23:04 +0200

    Create CrudService, add methods that repeated, implements in other interfaces. closes #11

--- pet-clinic-data/src/main/java/przemek/springlearn/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/przemek/springlearn/petclinic/services/OwnerService.java
@@ -1,15 +1,11 @@
 package przemek.springlearn.petclinic.services;
 
+import org.springframework.boot.web.reactive.filter.OrderedWebFilter;
 import przemek.springlearn.petclinic.model.Owner;
 
 import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner,Long>{
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-
 }
