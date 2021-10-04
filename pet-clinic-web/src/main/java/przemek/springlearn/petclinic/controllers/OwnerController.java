b202478ee8c39e0f3eec279e34369a4da03fdb58
przemek196 <przemekbilas@gmail.com>
2021-10-04 22:55:55 +0200

    Add owner index file and controller. closes #13

--- pet-clinic-web/src/main/java/przemek/springlearn/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/przemek/springlearn/petclinic/controllers/OwnerController.java
@@ -3,13 +3,14 @@ package przemek.springlearn.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@RequestMapping("owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets() {
-        return "vets/index";
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listOwners() {
+        return "owners/index";
     }
+
 
 }
