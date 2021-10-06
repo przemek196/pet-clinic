1581f9cf7f03262976d955e3aecc7b50d381ac25
przemek196 <przemekbilas@gmail.com>
2021-10-06 19:26:31 +0200

    list vets on vets index page. closes #15

--- pet-clinic-web/src/main/java/przemek/springlearn/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/przemek/springlearn/petclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package przemek.springlearn.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import przemek.springlearn.petclinic.services.VetService;
 
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets() {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 
