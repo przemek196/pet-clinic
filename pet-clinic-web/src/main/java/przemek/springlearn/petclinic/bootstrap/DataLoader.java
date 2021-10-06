9b28c23af0ceaaff08b3e6e356ee396c73275921
przemek196 <przemekbilas@gmail.com>
2021-10-06 22:39:02 +0200

    add auto increment id for map. closes #23

--- pet-clinic-web/src/main/java/przemek/springlearn/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/przemek/springlearn/petclinic/bootstrap/DataLoader.java
@@ -24,14 +24,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Jan");
         owner1.setLastName("Kowalski");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Bartosz");
         owner2.setLastName("Kapustka");
 
@@ -40,14 +38,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners.....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Barbara");
         vet1.setLastName("Laka");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Marzena");
         vet2.setLastName("Odpust");
 
